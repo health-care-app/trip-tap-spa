94c0707062790ab76c5636aa34fb6cc41867fb67
Hassan Khousheish <khousheish@live.com>
2020-07-25 11:24:46 +0300

    TRIP-23 Add NGRX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { reducers } from '@AppStore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +16,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot(reducers, {
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 30,
+    }),
   ],
   providers: [],
   bootstrap: [
