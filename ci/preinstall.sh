f5ac13252eb977408dac000e72d178b290dd5d92
Mostafa <mostafa@ionoview.com>
2020-08-24 18:17:36 +0300

    TRIP-4 Fix travis error

--- ci/preinstall.sh
+++ ci/preinstall.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env ash
+#!/usr/bin/env bash
 
 cp src/environments/environment.dist.ts src/environments/environment.ts
 cp src/environments/environment.prod.dist.ts src/environments/environment.prod.ts
